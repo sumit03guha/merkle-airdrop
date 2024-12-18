3c31a2a2a3292b0afe1cb4fd6034d7a1989a0f09
Sumit Guha <sumit03guha@gmail.com>
2024-12-19 01:12:34 +0530

    feat: added getters

--- src/contracts/MerkleAirdrop.sol
+++ src/contracts/MerkleAirdrop.sol
@@ -4,22 +4,23 @@ pragma solidity 0.8.28;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {MerkleAirdrop__AlreadyClaimed, MerkleAirdrop__InvalidProof} from "./Errors.sol";
 
-contract MerkleAirdrop {
+contract MerkleAirdrop is Ownable {
     using SafeERC20 for IERC20;
 
     bytes32 private _merkleRoot;
-    IERC20 private _token;
+    IERC20 private _airdropToken;
 
     mapping(address claimer => bool claimed) private _airdropClaimed;
 
     event AirdropClaimed(address claimer, uint256 amount);
 
-    constructor(bytes32 merkleRoot, address token) {
+    constructor(bytes32 merkleRoot, address token) Ownable(msg.sender) {
         _merkleRoot = merkleRoot;
-        _token = IERC20(token);
+        _airdropToken = IERC20(token);
     }
 
     function claim(
@@ -40,7 +41,19 @@ contract MerkleAirdrop {
         }
 
         _airdropClaimed[claimer] = true;
-        _token.safeTransfer(claimer, amount);
+        _airdropToken.safeTransfer(claimer, amount);
         emit AirdropClaimed(claimer, amount);
+    }
+
+    function isAirdropClaimed(address claimer) external view returns (bool) {
+        return _airdropClaimed[claimer];
+    }
+
+    function getAirdropToken() external view returns (address) {
+        return address(_airdropToken);
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return _merkleRoot;
     }
 }
