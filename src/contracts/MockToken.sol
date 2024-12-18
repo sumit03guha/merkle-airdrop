ffa3cbcc42e6fe4c9fd106b260f8372a0b4cc235
Sumit Guha <sumit03guha@gmail.com>
2024-12-19 00:48:19 +0530

    feat: implemented mock token

---
+++ src/contracts/MockToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockToken is ERC20, Ownable {
+    constructor() ERC20("MockToken", "MOK") Ownable(msg.sender) {}
+}
